5050eba9cb0dd9f6d37c27c2e2ca1899d661d9d3
Mohit Arun Inampudi <mi9.developer@gmail.com>
2017-05-31 20:37:58 -0400

    added TodoApp component

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory}= require('react-router');
 
+var TodoApp = require('TodoApp');
 
 //Foundation css
 $(document).foundation();
@@ -14,6 +15,6 @@ var message = 'This messsage is from Prop'
 
 ReactDOM.render(
 
-  <p>BoilerPlate3 </p>
+  <TodoApp />
 
   , document.getElementById('app'));
